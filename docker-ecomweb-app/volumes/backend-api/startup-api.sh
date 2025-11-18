0c0da32b3cf242cb31cc1728d1ffc116c89fdc32
travisnguyen20 <travisnguyen.me@gmail.com>
2025-11-18 23:01:30 +0700

    feat: sync

--- docker-ecomweb-app/volumes/backend-api/startup-api.sh
+++ docker-ecomweb-app/volumes/backend-api/startup-api.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Running Prisma migrations..."
-npx prisma migrate deploy
+echo "Using DATABASE_URL_NON_POOLING for migrations"
+DATABASE_URL="${DATABASE_URL_NON_POOLING}" npx prisma migrate deploy
 
 echo "Starting API service with PM2..."
 pm2 start /app/ecosystem.api.config.js --no-daemon
